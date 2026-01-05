e65940fad981497911b9374d5a72750f48608103
Antonio <you@example.com>
2026-01-05 13:58:41 +0100

    añadido main de prueba creando objeto de clase abstracta

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,21 +2,6 @@
 #include "Dog.hpp"
 #include "Cat.hpp"
 
-/* int main()
-{
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-	delete meta;
-	delete j;
-	delete i;
-} */
-
 int main()
 {
 	const AAnimal* j = new Dog();
@@ -77,4 +62,11 @@ int main()
 
 	return 0;
 }
+
+/* int main(void)
+{
+	AAnimal unicornio;
+
+	unicornio.makeSound;
+} */
 
